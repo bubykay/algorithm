c166083c5fc3a2fdaa947e95f853ff8be1eb8e71
Kayode Adetayo <bubykay@gmail.com>
2020-09-09 21:49:41 +0100

    freecodecamp compareTriblet practice added

---
+++ compareTriblet.js
@@ -0,0 +1,18 @@
+function compareTriblet(c,d) {
+    let result = [];
+    let a = 0;
+    let b = 0;
+    for(let i = 0; i<=2; i++){
+        if(c[i]>d[i]){
+            a += 1
+        }else if(c[i]<d[i]){
+            b += 1
+        }
+    }
+    result.push(a);
+    result.push(b);
+    return result.join('')
+    
+}
+
+compareTriblet([17,28,30],[99,16,8])
