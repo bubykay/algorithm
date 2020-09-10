0770d71f3767bbd447b7f6def83f6a808b520e6d
Kayode Adetayo <bubykay@gmail.com>
2020-09-10 05:48:13 +0100

    added diagonalDifference

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,8 +1,12 @@
-function diagonalDifference (arr){
-    let mainSum = 0, secondarySum = 0;
-    for(let i=0; i=arr.length-1; i++){
-      let kk = arr[i][i];
-      return kk;
-    }
+//returns diagonal difference of a square matrix
+function diagonalDifference (matrix){
+  let diagonal1 = 0, diagonal2 = 0;
+
+  for (let row = 0; row < matrix.length; row++) {
+      diagonal1 += matrix[row][row];
+      diagonal2 += matrix[row][matrix.length - row - 1];
+  }
+  let difference = diagonal1 - diagonal2;
+  return Math.abs(difference)
 }
 console.log(diagonalDifference([[1,2,7],[3,4,8],[5,6,9]]))
